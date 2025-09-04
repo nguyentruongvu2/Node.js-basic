b8851a7b58e8f8ff3d7d04d1d3af2e177e89a4e3
Truongvu <nguyentruongvu2023@gmail.com>
2025-09-04 21:23:36 +0700

    server express

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
-let http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World!');
-}).listen(8080);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World !')
+})
+app.get('/about', (req, res) => {
+    res.send('Hello bạn! Mình là Vũ')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
